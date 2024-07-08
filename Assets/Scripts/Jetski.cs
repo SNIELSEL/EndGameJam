c8fa9f496fea5cca6fccc63239f83de910b3a568
Doetzen <Doutzenhoekstra06@gmail.com>
2024-07-08 12:58:11 +0200

    JEtski

--- Assets/Scripts/Jetski.cs
+++ Assets/Scripts/Jetski.cs
@@ -1,72 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jetski : MonoBehaviour
 {
-/*    private PlayerActionMap playerActionMap;
-    private InputAction move;
+
+    public Rigidbody rb;
+    public GameObject jetskiBody;
+
+    public float speed;
 
 
-    [HideInInspector] public Rigidbody rb;
-    [SerializeField] float moveSpeed, normalSpeed;
-    [SerializeField] float maxSpeed;
+    public Transform right, left, straigt;
 
-    public Vector3 forceDirection;
-
-    private void Awake()
+    private void Start()
     {
-        playerActionMap = new PlayerActionMap();
+        rb = GetComponent<Rigidbody>();
 
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        normalSpeed = moveSpeed;
-
-        //miss hier alle stats van de player aangeven ofzo
     }
 
-    private void OnEnable()
+
+    private void Update()
     {
-        move = playerActionMap.Character.Move;
-        playerActionMap.Character.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerActionMap.Character.Disable();
-    }
-
-    private void FixedUpdate()
-    {
-        forceDirection += move.ReadValue<Vector2>().x * moveSpeed;
-        forceDirection += move.ReadValue<Vector2>().y * moveSpeed;
-
-        rb.AddForce(forceDirection, ForceMode.Impulse);
-        forceDirection = Vector3.zero;
-
-
-       *//* if (rb.velocity.y < 0)
+        if(Input.GetKey("w"))
         {
-            if (gliding)
-            {
-                rb.velocity -= Vector3.down * Physics.gravity.y * 0.1f * Time.fixedDeltaTime;
-
-            }
-            else
-            {
-                rb.velocity -= Vector3.down * Physics.gravity.y * 3.5f * Time.fixedDeltaTime;
-            }
-        }*//*
-        //als ik wil dat je soort van kunt gliden ofzo dan kun je net zoeits doen ^^
-
-        Vector3 horizontalVel = rb.velocity;
-        horizontalVel.y = 0;
-        if (horizontalVel.sqrMagnitude > maxSpeed * maxSpeed)
-        {
-            rb.velocity = horizontalVel.normalized * maxSpeed + Vector3.up * rb.velocity.y;
+            rb.velocity += transform.forward * 100 * Time.deltaTime;
         }
-        //LayerMask layerMask = 1 << 9;
-    }*/
+
+        if (Input.GetKey("s"))
+        {
+            rb.velocity -= transform.forward * 100 * Time.deltaTime;
+        }
+
+        if(Input.GetKey("a") && Input.GetKey("w"))
+        {
+            transform.Rotate(0, -30 * Time.deltaTime, 0);
+            jetskiBody.transform.rotation = Quaternion.Lerp(jetskiBody.transform.rotation, left.rotation, 2 * Time.deltaTime);
+            rb.velocity += jetskiBody.transform.forward * 70 * Time.deltaTime;
+            rb.velocity -= transform.forward * 70 * Time.deltaTime;
+        }
+
+        if(Input.GetKey("d") && Input.GetKey("w"))
+        {
+            transform.Rotate(0, 30 * Time.deltaTime, 0);
+            jetskiBody.transform.rotation = Quaternion.Lerp(jetskiBody.transform.rotation, right.rotation,2 * Time.deltaTime);
+            rb.velocity += jetskiBody.transform.forward * 70 * Time.deltaTime;
+            rb.velocity -= transform.forward * 70 * Time.deltaTime;
+        }
+
+
+        if(!Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            jetskiBody.transform.rotation = Quaternion.Lerp(jetskiBody.transform.rotation, straigt.rotation, 2 * Time.deltaTime);
+        }
+    }
+
+    /* private JetskiInputActions input;
+     public Rigidbody rb;
+     public GameObject camHold;
+     public float speed, sprint;
+
+     private Vector2 move, look;
+
+     private InputAction movew, rotate;
+     private float x, y;
+
+     private void Awake()
+     {
+         input = new JetskiInputActions();
+         rb = GetComponent<Rigidbody>();
+
+         Cursor.lockState = CursorLockMode.Locked;
+         Cursor.visible = false;
+     }
+
+     private void OnEnable()
+     {
+         movew = input.JetskiMap.Move;
+         movew.Enable();
+
+
+     }
+
+     private void OnDisable()
+     {
+         movew.Disable();
+     }
+
+     private void Update()
+     {
+         Rotate();
+     }
+
+     private void FixedUpdate()
+     {
+         Move();
+     }
+
+     private void Move()
+     {
+         move = movew.ReadValue<Vector2>() * (speed * Time.deltaTime);
+
+         transform.Translate(move.x, 0, move.y);
+     }
+
+     private void Rotate()
+     {
+         //look = rotate.ReadValue<Vector2>();
+
+
+         *//*   x += look.x * sens;
+            y -= look.y * sens;*//*
+
+         //y = Mathf.Clamp(y, -85, 85);
+         transform.LookAt(transform.position, transform.forward);
+         //transform.localRotation = Quaternion.Euler(0, x, 0 * Time.deltaTime);
+         //camHold.transform.localRotation = Quaternion.Euler(y, 0, 0 * Time.deltaTime);
+     }*/
 }
